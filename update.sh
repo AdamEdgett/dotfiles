c1e833311ca40e1dedec43f33f818366688315c0
Adam Edgett <AdamEdgett@gmail.com>
2013-09-22 12:51:33 -0400

    Added update script

---
+++ update.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# Self
+read -p "Do you want to update this project? [Y/n] " -n 1
+echo ""
+if [[ $REPLY  =~ ^[Yy]?$ ]]
+then
+    cd ~/.dotfiles
+    if git pull
+    then
+        echo "Updated this project"
+    fi
+fi
+
+# Oh-My-Zsh
+read -p "Do you want to update Oh-My-Zsh? [Y/n] " -n 1
+echo ""
+if [[ $REPLY  =~ ^[Yy]?$ ]]
+then
+    cd ~/.zsh
+    if git pull
+    then
+        echo "Updated Oh-My-Zsh"
+    fi
+fi
+
+# Vundle
+read -p "Do you want to update vim plugins? [Y/n] " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]?$ ]]
+then
+    if vim +BundleUpdate +qall
+    then
+        echo "Updated vim plugins"
+    fi
+fi

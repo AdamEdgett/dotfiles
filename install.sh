cb561870e46a545b9b1094631f2547292f5359de
Adam Edgett <AdamEdgett@gmail.com>
2014-02-04 14:15:44 -0500

    Removed unneeded linebreaks in install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,14 @@
 # Oh-My-Zsh
 read -p "Do you want to install Oh-My-Zsh? [Y/n] " -n 1
 echo ""
-if [[ $REPLY  =~ ^[Yy]?$ ]]
-then
+if [[ $REPLY  =~ ^[Yy]?$ ]] then
     git clone --quiet  https://github.com/robbyrussell/oh-my-zsh.git ~/.zsh
 fi
 
 # Zsh
 read -p "Do you want to setup config files for zsh? [Y/n] " -n 1
 echo ""
-if [[ $REPLY =~ ^[Yy]?$ ]]
-then
+if [[ $REPLY =~ ^[Yy]?$ ]] then
     ln -s $PWD/zsh/zshrc ~/.zshrc
     ln -s $PWD/zsh/aliases.zsh ~/.zsh/custom/aliases.zsh
     ln -s $PWD/zsh/custom.zsh-theme ~/.zsh/custom/custom.zsh-theme
@@ -21,8 +19,7 @@ fi
 # Git
 read -p "Do you want to setup config files for git? [Y/n] " -n 1
 echo ""
-if [[ $REPLY =~ ^[Yy]?$ ]]
-then
+if [[ $REPLY =~ ^[Yy]?$ ]] then
     ln -s $PWD/git/gitconfig ~/.gitconfig
     ln -s $PWD/git/gitignore ~/.gitignore
 fi
@@ -30,8 +27,7 @@ fi
 # Vundle
 read -p "Do you want to install vundle and vim plugins? [Y/n] " -n 1
 echo ""
-if [[ $REPLY =~ ^[Yy]?$ ]]
-then
+if [[ $REPLY =~ ^[Yy]?$ ]] then
     mkdir -p ~/.vim/bundle
     git clone --quiet https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
     vim +BundleInstall +qall
@@ -40,8 +36,7 @@ fi
 # Vim
 read -p "Do you want to setup config files for vim? [Y/n] " -n 1
 echo ""
-if [[ $REPLY =~ ^[Yy]?$ ]]
-then
+if [[ $REPLY =~ ^[Yy]?$ ]] then
     mkdir -p ~/.vim
     ln -s $PWD/vim/vimrc ~/.vimrc
     ln -s $PWD/vim/ftplugin ~/.vim/ftplugin
@@ -50,8 +45,7 @@ fi
 # Xmonad
 read -p "Do you want to setup config files for xmonad? [Y/n] " -n 1
 echo ""
-if [[ $REPLY =~ ^[Yy]?$ ]]
-then
+if [[ $REPLY =~ ^[Yy]?$ ]] then
     ln -s $PWD/xmonad/xinitrc ~/.xinitrc
     ln -s $PWD/xmonad/xmonad.hs ~/.xmonad/xmonad.hs
     ln -s $PWD/xmonad/xmobarrc ~/.xmobarrc

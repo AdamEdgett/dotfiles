8a4e181fb077f7be0cf127e295574baf0b89215c
Adam Edgett <AdamEdgett@gmail.com>
2018-10-09 08:35:07 -0400

    Update update script

--- update.sh
+++ update.sh
@@ -4,20 +4,9 @@ read -p "Do you want to update this project? [Y/n] " -n 1
 echo ""
 if [[ $REPLY  =~ ^[Yy]?$ ]]; then
     cd ~/.dotfiles
-    if git pull
+    if git pull --recurse-submodules
     then
         echo "Updated this project"
-    fi
-fi
-
-# Oh-My-Zsh
-read -p "Do you want to update Oh-My-Zsh? [Y/n] " -n 1
-echo ""
-if [[ $REPLY  =~ ^[Yy]?$ ]]; then
-    cd ~/.zsh
-    if git pull
-    then
-        echo "Updated Oh-My-Zsh"
     fi
 fi
 
